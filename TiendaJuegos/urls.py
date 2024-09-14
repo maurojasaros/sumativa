f00734e1a985937d572dc3523db968b1a9c5ac2c
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2024-09-13 21:13:47 -0300

    Estoy creando en models las clases, de momento solo tengo Juego y Categorias

--- TiendaJuegos/urls.py
+++ TiendaJuegos/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('juegos.urls')),
+    path('juegos/', include('juegos.urls')),
+    path('', lambda request: redirect('juegos/')),
 ]
--- juegos/models.py
+++ juegos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Juego(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='juegos', default=1, null=False)
+
+    def __str__(self):
+        return self.nombre
