52baf55854f0ac9ad17366200f123e4b7d6f9b57
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2024-09-13 21:36:08 -0300

    Agregué mas clases, para hacer las tablas suficientes para el modelo MER, me quedó la duda con el valor por defecto de la clase Categoria

--- juegos/models.py
+++ juegos/models.py
@@ -14,6 +14,45 @@ class Juego(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='juegos', default=1, null=False)
+#Tengo que considerar que siempre debo tener una categoria valida antes de crear un juego
+#Quise usar un valor predeterminado que en este caso es 1
+    def __str__(self):
+        return self.nombre
+    
+class TipoUsuario(models.Model):
+    descripcion = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.descripcion
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    direccion = models.TextField()
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nombre
+
+class Carrito(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f"Carrito de {self.usuario.nombre}"
+
+class ItemCarrito(models.Model):
+    carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
+    juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.juego.nombre} en carrito"
+
+class Pedido(models.Model):
+    carrito = models.OneToOneField(Carrito, on_delete=models.CASCADE)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return f"Pedido realizado el {self.fecha_pedido}"
